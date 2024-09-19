9b35d116823c9c4fbce93abcec98fbe4b12e94d7
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2024-09-19 13:48:09 +0200

    Added code

---
+++ midas_model.py
@@ -0,0 +1,47 @@
+import cv2
+import torch
+import urllib.request
+
+import matplotlib.pyplot as plt
+
+url, filename = ("https://images.unsplash.com/photo-1463748465553-80db1e6ff830?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8NHx8fGVufDB8fHx8fA%3D%3D", "horses.jpg")
+urllib.request.urlretrieve(url, filename)
+
+# model_type = "DPT_Large"     # MiDaS v3 - Large     (highest accuracy, slowest inference speed)
+# model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid    (medium accuracy, medium inference speed)
+model_type = "MiDaS_small"  # MiDaS v2.1 - Small   (lowest accuracy, highest inference speed)
+
+midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+midas.to(device)
+midas.eval()
+
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+    transform = midas_transforms.dpt_transform
+else:
+    transform = midas_transforms.small_transform
+    
+img = cv2.imread(filename)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+input_batch = transform(img).to(device)
+
+with torch.no_grad():
+    prediction = midas(input_batch)
+
+    prediction = torch.nn.functional.interpolate(
+        prediction.unsqueeze(1),
+        size=img.shape[:2],
+        mode="bicubic",
+        align_corners=False,
+    ).squeeze()
+
+output = prediction.cpu().numpy()
+
+plt.imshow(output)
+plt.axis('off')
+plt.savefig('output_depth.png')
+plt.show()
